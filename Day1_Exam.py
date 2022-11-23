af0196dda52656ea8dcf79a77b6039b1502bace6
Magnus <dorfmagnus@gmail.com>
2022-11-23 15:23:21 +0300

    Day1 Commit

---
+++ Day1_Exam.py
@@ -0,0 +1,30 @@
+# Example Output
+
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# print("I say: \nYou say:")
+## I say: 
+## You say:
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+#Write your code below this line 👇
+
+# name = input("What is your name? : ")
+# print(len(name))
+
+# name = input("Hello" + "What is your name? : ")
+# print("Hello" + "What is your name? : ")
+
+# hello = "I am Here"
+# print(hello)
+
+# print('Hello ' + input("Your name? :"))
