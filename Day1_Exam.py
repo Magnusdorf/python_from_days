45705aef515a98e2ac3a047a26b7c98e839d27be
Magnus <dorfmagnus@gmail.com>
2022-11-23 16:37:02 +0300

    Day1 End

--- Day1_Exam.py
+++ Day1_Exam.py
@@ -21,6 +21,8 @@
 # name = input("What is your name? : ")
 # print(len(name))
 
+# print(len(input("What is your name ? : ")))
+
 # name = input("Hello" + "What is your name? : ")
 # print("Hello" + "What is your name? : ")
 
@@ -28,3 +30,65 @@
 # print(hello)
 
 # print('Hello ' + input("Your name? :"))
+
+
+# # 🚨 Don't change the code below 👇
+# x = input("x: ")
+# y = input("y: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+
+# z = x 
+# x = y
+# y = z
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("x: " + x)
+# print("y: " + y)
+
+
+# name = 'Angela'
+# print(name)
+
+# print("Hello")
+
+# print('hello')
+
+# num = '5'
+# print("Tere are "+ num +" hour")
+
+
+
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+
+# print("-Welcome to the Band Name Generator.")
+# city = input("What's the name of the city you grew up in ?\n")
+# print(city)
+# pet = input("What's your pet's name?\n")
+# print(pet)
+# print(f"Your band name could be {city} {pet}.")
+
+# print("-Welcome to the Band Name Generator.")
+
+# city = input("What's the name of the city you grew up in ?")
+# print(city)
+# pet = input("What's your pet's name?")
+# print(pet)
+# print("Your band name could be " + city + ' ' + pet)
+
+
+## End..
