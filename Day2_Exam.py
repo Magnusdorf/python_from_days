c2889a0d51620cec3b64bf513e7dfd9cb113cd13
Magnus <dorfmagnus@gmail.com>
2022-11-24 18:55:40 +0300

    Day2 started

---
+++ Day2_Exam.py
@@ -0,0 +1,53 @@
+#project
+
+# bill = float(input("What was the total bill? "))
+# people = float(input("How many people to split the bill? "))
+# percentage = float(input("What percentage tip would you like to given? "))
+# total_bill = float((bill + percentage) / people)
+# print(f"amount to be paid per person : {total_bill} ")
+
+#end
+
+#String
+
+# print("Hello"[0])
+
+# say =  "Hello"
+# print(say[3])
+
+#Integer
+
+# print(123 + 123)
+
+# print(123_13_13)
+
+#Float
+
+# num_pi = 3.14
+# print(num_pi, type(num_pi))
+
+#Boolen  True/False
+
+# num_char = len(input("What is your name? "))
+# num_char = str(num_char) # new_num_char = str(num_char)
+# print("Your anme has " + num_char + " characters.")
+# print(type(num_char))
+
+# print(55 + float("565.5"))
+# print("Hi " + "Boy")
+
+
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 34, then the output should be 3 + 4 = 7
+
+two_digit_number = input("Type a two digit number: ")
+
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+first_digit = int(first_digit)
+second_digit = int(second_digit)
+## result = int(first_digit) + int(second_ digit)
+##print(result)
+print(first_digit + second_digit)
+
+
