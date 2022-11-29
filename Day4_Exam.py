30c038fefd59907674da758adee6b0f343a247ec
Magnus <dorfmagnus@gmail.com>
2022-11-29 22:47:23 +0300

    Day4 Complated

--- Day4_Exam.py
+++ Day4_Exam.py
@@ -69,3 +69,81 @@
 # random_name = random.randint(0, x - 1)
 # person_pay = names[random_name]
 # print(person_pay + "is going to buy the meal today ")
+
+# on't change the code below 
+
+
+# row1 = ["⬜️","️⬜️","️⬜️"]
+# row2 = ["⬜️","⬜️","️⬜️"]
+# row3 = ["⬜️️","⬜️️","⬜️️"]
+# map = [row1, row2, row3]
+# print(f"{row1}\n{row2}\n{row3}")
+
+# position = input("Where do you want to put the treasure? ")
+# horizonal = int(position[0])
+# vertical = int(position[1])
+
+# selected_row = map[vertical - 1]
+# selected_row[horizonal - 1] = 'X'
+
+# #Write your code above this row 
+
+# # on't change the code below 
+# print(f"{row1}\n{row2}\n{row3}")
+
+
+# project #
+
+# import random
+
+# rock = '''
+#     _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)
+# '''
+
+# paper = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#           _______)
+#          _______)
+# ---.__________)
+# '''
+
+# scissors = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+# '''
+
+# game_images = [rock, paper, scissors]
+# user_choice = int(input("What do you choose ? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+# print(game_images[user_choice])
+
+# computer_choice = random.randint(0, 2)
+# print(f"Computer chose \n{computer_choice}")
+# print(game_images[computer_choice])
+
+# if user_choice >= 3 :
+#     print("You typed an invalid number, you lose!")
+# elif user_choice == 0 and computer_choice == 2 :
+#     print("You win!")
+# elif computer_choice == 0 and user_choice == 2:
+#     print("You lose!")
+# elif computer_choice > user_choice :
+#     print("You Lose!")
+# elif user_choice > computer_choice :
+#     print("You win!")
+# elif computer_choice == user_choice:
+#     print("It's a draw")
+# elif user_choice >= 3 :
+#     print("You typed an invalid number, you lose!")
+# else :
+#     print("You typed an invalid number, you lose! ")
