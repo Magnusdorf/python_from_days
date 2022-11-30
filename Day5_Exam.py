c0a5e51fa7bc787545c0604f75267a08f328edc6
Magnus <dorfmagnus@gmail.com>
2022-11-30 16:26:27 +0300

    Day5

---
+++ Day5_Exam.py
@@ -0,0 +1,37 @@
+# fruits = ['Apple', 'Peach', 'Pear']
+
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + ' Pie')
+
+# You are going to write a program that calculates the average student height from a List of heights.
+
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
+
+# total_heights = 0
+# for height in student_heights:
+#     total_heights += height
+# # print(total_heights)
+
+# number_of_students = 0
+# for students in student_heights:
+#     number_of_students += 1
+# # print(number_of_students)
+
+# average_height = round(total_heights / number_of_students)
+# print(average_height)
+
+
+
+# student_scores = input("Input a list of student scores ").split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+# print(student_scores)
+
+# highest_score = 0
+# for score in student_scores:
+#     if score > highest_score:
+#         highest_score = score
+# print(f"The highest score in the class is : {highest_score}")
